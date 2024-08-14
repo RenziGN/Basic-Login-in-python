5476e5b913540c5e7843e4f2a7e509c7d6bee187
Renzo Gola <112965715+RenziGN@users.noreply.github.com>
2024-08-14 14:42:00 -0300

    Update Login Basico.py

--- Login Basico.py
+++ Login Basico.py
@@ -4,12 +4,12 @@ Users = {
 
 def NuevoUsuario():
     username = str(input("A continuacion ingrese un nombre de usuario: "))
-    while username.isspace() == True: #Este programa tiene como agregado el uso de isspace, ya que si un usuario utilizara Espacios unicamente le permitiria crear un usuario en blanco
+    while username.isspace() == True or username =="": #Este programa tiene como agregado el uso de isspace y comprueba si es unicamente un espacio en blanco, ya que si un usuario utilizara Espacios unicamente le permitiria crear un usuario en blanco
         print("Su usuario no puede estar vacio ")
         username = str(input("A continuacion ingrese un nombre de usuario: "))
     else: pass
     password = str(input("A continuacion ingrese la contraseña para este usuario: "))
-    while password.isspace() == True:
+    while password.isspace() == True or username == "":
         print("Su contraseña no puede estar vacia ")
         password = str(input("A continuacion ingrese la contraseña para este usuario: "))
     else: pass
@@ -26,12 +26,12 @@ def MostrarUsuarios():
 def login():
     while True:
             nombredeusuario = str(input("Ingrese su usuario: "))
-            while nombredeusuario.isspace() == True:
+            while nombredeusuario.isspace() == True or nombredeusuario =="":
                  print("No existe un usuario vacio")
                  nombredeusuario = str(input("Ingrese su usuario: "))
             else: pass
             contraseña = str(input("Ingrese su contraseña: "))
-        
+
             if nombredeusuario in Users:
                 if Users[nombredeusuario] == contraseña:
                     print(f"Bienvenido al sistema {nombredeusuario}")
@@ -40,4 +40,4 @@ def login():
                     print("Contraseña Incorrecta, intentelo nuevamente")
             else:
                  print("Este usuario no se encuentra registrado en el sistema")
-                 return                 
+                 break
