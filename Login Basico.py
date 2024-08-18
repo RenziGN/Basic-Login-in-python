9ad75bd831333fbbd07cdfc85320b069441e1e70
Renzo Gola <112965715+RenziGN@users.noreply.github.com>
2024-08-18 19:45:18 -0300

    Update Login Basico.py
    Este código sigue recibiendo actualizaciones para mejorar su eficiencia.

--- Login Basico.py
+++ Login Basico.py
@@ -4,17 +4,23 @@ Users = {
 
 def NuevoUsuario():
     username = str(input("A continuacion ingrese un nombre de usuario: "))
-    while username.isspace() == True or username =="": #Este programa tiene como agregado el uso de isspace y comprueba si es unicamente un espacio en blanco, ya que si un usuario utilizara Espacios unicamente le permitiria crear un usuario en blanco
-        print("Su usuario no puede estar vacio ")
-        username = str(input("A continuacion ingrese un nombre de usuario: "))
-    else: pass
-    password = str(input("A continuacion ingrese la contraseña para este usuario: "))
-    while password.isspace() == True or password == "":
-        print("Su contraseña no puede estar vacia ")
+    if username in Users:
+        print("Este usuario ya existe")
+    else:
+        while (username == "") or (" " in username):
+            print("Su usuario no puede estar vacio o contener espacios ")
+            username = str(input("A continuacion ingrese un nombre de usuario: "))
+        else: pass
         password = str(input("A continuacion ingrese la contraseña para este usuario: "))
-    else: pass
-    print("Usuario Registrado correctamente")
-    Users[username] = password
+        while (password == "") or (" " in password):
+            print("Su contraseña no puede estar vacia o contener espacios ")
+            password = str(input("A continuacion ingrese la contraseña para este usuario: "))
+        else:
+            if password == username:
+                print("Su Contraseña no puede ser igual al usuario")
+            else:
+                print("Usuario Registrado correctamente")
+        Users[username] = password
 
 
 
@@ -26,7 +32,7 @@ def MostrarUsuarios():
 def login():
     while True:
             nombredeusuario = str(input("Ingrese su usuario: "))
-            while nombredeusuario.isspace() == True or nombredeusuario =="":
+            while (nombredeusuario == "") or (" " in nombredeusuario):
                  print("No existe un usuario vacio")
                  nombredeusuario = str(input("Ingrese su usuario: "))
             else: pass
